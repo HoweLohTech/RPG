78dc2017360aa31d6291aa3e8db37b685a89310e
ghandi263 <ghandi263@howeloh.com>
2015-01-16 05:54:17 +0000

    insertUser near complete
    added a bunch of stuff to insertUser. Now it needs to disconnect from the database and other stuff.

--- insertUser.php
+++ insertUser.php
@@ -5,6 +5,20 @@ require "dbConnect.php";
 if ($_POST["NewUPass"] != $_POST["NewUPassConf"]){
     die('Passwords do not match');
 }
+<<<<<<< Updated upstream
+=======
+
+$NewUsername = mysql_real-escape_string(testFormData($_Post['NewUsername']));
+$NewUPass = hash('sha256', mysql_real_escape_string(testFormData($_POST["NewUPass"])));
+$NewUFirst = mysql_real-escape_string(testFormData($_Post['NewUFirst']));
+$NewULast = mysql_real-escape_string(testFormData($_Post['$NewULast']));
+$NewUBirth = mysql_real-escape_string(testFormData($_Post['NewUBirth']));
+
+$sqlIns= 'INSERT INTO users ' .
+    "(id, username, password, firstName, lastName, bDate)" .
+    "Values ('', '$NewUsername', '$NewUPass', '$NewUFirst', '$NewULast', '$NewUBirth')";
+    
+>>>>>>> Stashed changes
 
 $NewUsername= mysql_real-escape_string(testFormData($_Post['NewUsername']));
 $NewUPass= hash('sha256', mysql_real_escape_string(testFormData($_POST["NewUPass"])));
