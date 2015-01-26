450fb3942fcdb7cd2105fc89997d45eac84ef568
Eric Lohmar <ghandi263@howloh.com>
2015-01-26 17:42:05 -0600

    Started Eric's Resume Subdomain
    Started the index, a header, and a stylesheet.

---
+++ subdomains/eric/Header Footer/Header.php
@@ -0,0 +1,16 @@
+<?php
+echo "<div class='selectors'>
+		<ul>
+			<li>
+				<div><a href='index.php'>About Me<br>(General Info)<a></div>
+			<li>
+				<div><a href='education.php'>Education</a></div>
+			</li>
+			<li>
+				<div><a href='history.php'>Work History</a></div>
+			</li>
+			<li>
+				<div><a href='achieve.php'>Achievements</a></div>
+		</ul>
+	</div>"
+?>
---
+++ subdomains/eric/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="resume style.css" rel="stylesheet"/>
+	<title>Eric Edward Lohmar</title>
+
+</head>
+<body>
+<?php include "Header Footer/header.php" ?>
+	<h1> This is a resume!</h1>
+</body>
