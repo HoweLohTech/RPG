3525223bbcec8f683c2d54e5ea78379942c49475
Eric Lohmar <ghandi263@howloh.com>
2015-01-27 06:45:27 -0600

    updated Resume Header
    working on making the title center and making things line up quite
    rightly.

--- subdomains/eric/Header Footer/Header.php
+++ subdomains/eric/Header Footer/Header.php
@@ -1,16 +1,20 @@
 <?php
-echo "<div class='selectors'>
-		<ul>
-			<li>
-				<div><a href='index.php'>About Me<br>(General Info)<a></div>
-			<li>
-				<div><a href='education.php'>Education</a></div>
-			</li>
-			<li>
-				<div><a href='history.php'>Work History</a></div>
-			</li>
-			<li>
-				<div><a href='achieve.php'>Achievements</a></div>
-		</ul>
+echo "<div id='header'>
+		<h1 id='title'>Eric Edward Lohmar<br></h1>
+		<div class='selectors'>
+			<ul>
+				<li>
+					<div><a href='index.php'>About Me<br>(General Info)<a></div>
+				</li>
+				<li>
+					<div><a href='education.php'>Education</a></div>
+				</li>
+				<li>
+					<div><a href='history.php'>Work History</a></div>
+				</li>
+				<li>
+					<div><a href='achieve.php'>Achievements</a></div>
+			</ul>
+		</div>
 	</div>"
 ?>
